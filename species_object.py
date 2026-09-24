4ec1b5adc03371387b8dca88085ecb7df9d46930
s-tjong <shaetjong@gmail.com>
2026-09-24 10:35:37 +0100

    Initial Commit

---
+++ species_object.py
@@ -0,0 +1,5 @@
+class Species:
+    def __init__(self, species_name, species_population, species_type):
+        self.species_name = species_name
+        self.species_population = species_population
+        self.species_type = species_type
